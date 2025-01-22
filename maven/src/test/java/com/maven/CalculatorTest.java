b2bd66fbf52d77ca01c3c5d60e7734a39eef58fa
Smaster21 <safumaster9221@gmail.com>
2025-01-22 12:38:38 +0530

    first maven

---
+++ maven/src/test/java/com/maven/CalculatorTest.java
@@ -0,0 +1,38 @@
+package com.maven;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    private static Calculator calculator;
+
+    @BeforeAll
+    static void setUp(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    void addSimple(){
+        double result = calculator.calculate(1,1,'+');
+        assertEquals(2, result);
+    }
+
+    @Test
+    void multiplySimple(){
+        double result = calculator.calculate(1,1,'*');
+        assertEquals(1, result);
+    }
+
+    @Test
+    void subtractSimple(){
+        double result = calculator.calculate(1,1,'-');
+        assertEquals(0, result);
+    }
+
+    @Test
+    void divideSimple(){
+        double result = calculator.calculate(1,1,'/');
+        assertEquals(1, result);
+    }
+}
